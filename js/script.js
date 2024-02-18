1fcc8cb42f717a84c4071775ac4859e888dfee3f
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-02-18 22:06:26 +0600

    discount calculation fixed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 const seatButtons = document.querySelectorAll("#first-seat-row button");
 const seatButtons2 = document.querySelectorAll("#second-seat-row button");
-const selectedSeats = [];
 let seatsSelected = 0;
-function handleSeatClick(btn) {
+const selectedSeats = [];
 
+// Add event listeners to seat buttons
+for (const button of seatButtons) {
+    button.addEventListener("click", function () {
+        handleSeatClick(button);
+    });
+}
+for (const button of seatButtons2){
+    button.addEventListener("click", function () {
+        handleSeatClick(button);
+    });
+}
+
+function handleSeatClick(btn) {
     if (btn.getAttribute('data-added') === 'true') {
         btn.removeAttribute('data-added');
         btn.style.backgroundColor = ""; 
@@ -12,7 +24,7 @@ function handleSeatClick(btn) {
         if (index !== -1) {
             selectedSeats.splice(index, 1); 
         }
-        seatsSelected--;
+        seatsSelected --;
         const seatAvailable = parseInt(document.getElementById("seat-count").innerText);
         document.getElementById("seat-count").innerText = seatAvailable + 1;
         const seatAvailable2 = parseInt(document.getElementById("seat-count-2").innerText);
@@ -21,14 +33,20 @@ function handleSeatClick(btn) {
 
         const seatCount = parseInt(document.getElementById("seat-count-2").innerText) ;
        const ticketPrice = document.getElementById("price").innerText = seatCount * 550;
-       const totalPrice = document.getElementById("total-price").innerText = ticketPrice
-
+       const totalPrice = document.getElementById("total-price").innerText = ticketPrice;
+       document.getElementById("discounted-price").classList.add("hidden");
+       document.getElementById("coupon-sec").classList.remove("hidden");
+       document.getElementById("coupon-error").classList.remove("hidden")
+       document.getElementById("coupon-error").innerText = "To get discount you have to purchase 4 tickets";
+       document.getElementById("apply-coupon").classList.add("bg-gray-700");
+       document.getElementById("apply-coupon").setAttribute('disabled','disabled');
     } else if (seatsSelected < 4) {
         btn.setAttribute('data-added', 'true');
         btn.style.backgroundColor = "#1dd100";
         btn.classList.add("text-white");
         selectedSeats.push(btn);
-        seatsSelected++;
+        seatsSelected ++;
+        
         const seatAvailable = parseInt(document.getElementById("seat-count").innerText);
         document.getElementById("seat-count").innerText = seatAvailable - 1;
         const seatAvailable2 = parseInt(document.getElementById("seat-count-2").innerText);
@@ -47,18 +65,39 @@ function handleSeatClick(btn) {
         const seatCount = parseInt(document.getElementById("seat-count-2").innerText) ;
        const ticketPrice = document.getElementById("price").innerText = seatCount * 550;
        const totalPrice = document.getElementById("total-price").innerText = ticketPrice;
+       if(seatCount !== 4){
+        document.getElementById("apply-coupon").setAttribute('disabled','disabled');
+        document.getElementById("total-price").innerText = totalPrice;
+        document.getElementById("apply-coupon").classList.add("bg-gray-700");
+        
+
+    }else{
+        discountCalc("NEW15", 15);
+        discountCalc("Couple 20", 20);
+        document.getElementById("apply-coupon").removeAttribute('disabled');
+        document.getElementById("apply-coupon").classList.remove("bg-gray-700");
+        document.getElementById("coupon-error").classList.add("hidden");
+    }
 
     }
 }
 
-// Add event listeners to seat buttons
-for (const button of seatButtons) {
-    button.addEventListener("click", function () {
-        handleSeatClick(button);
+function discountCalc(couponCode, discount) {
+    const applyCouponButton = document.getElementById("apply-coupon");
+    applyCouponButton.addEventListener('click', function() {
+        const couponText = document.getElementById("coupon-field").value;
+        if (couponText === couponCode) {
+            const totalPrice = parseInt(document.getElementById("price").innerText);
+            const discountedPrice = totalPrice / 100 * discount;
+            const grandTotal = totalPrice - discountedPrice;
+            document.getElementById("discounted-price").classList.remove("hidden")
+            document.getElementById("discounted-price").innerText = `- ${discountedPrice}`;
+            document.getElementById("total-price").innerText = grandTotal;
+            document.getElementById("coupon-sec").classList.add("hidden");
+            document.getElementById("coupon-error").classList.add("hidden");
+        } else {
+            document.getElementById("coupon-error").innerText = "Invalid coupon code";
+        }
     });
 }
-for (const button of seatButtons2){
-    button.addEventListener("click", function () {
-        handleSeatClick(button);
-    });
-}
+
